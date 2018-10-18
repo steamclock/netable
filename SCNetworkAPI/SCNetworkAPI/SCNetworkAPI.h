a1d8ffe7d80a41e3933d3d80ebf8422554887a87
Brendan Lensink <blensink192@gmail.com>
2018-10-18 13:28:19 -0700

    lock down alamofire version

---
+++ SCNetworkAPI/SCNetworkAPI/SCNetworkAPI.h
@@ -0,0 +1,12 @@
+//
+//  SCNetworkAPI.h
+//  SCNetworkAPIMobile
+//
+//  Created by Brendan Lensink on 2018-10-18.
+//  Copyright © 2018 steamclock. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double SCNetworkAPIVersionNumber;
+FOUNDATION_EXPORT const unsigned char SCNetworkAPIVersionString[];
