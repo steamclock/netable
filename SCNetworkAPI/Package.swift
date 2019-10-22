6aab5a291a0399487ba9c49f7bf0f049c87432f5
Jeremy Chiang <jeremy@steamclock.com>
2019-10-22 14:52:02 -0700

    Add Swift Package Manager manifest, #9

---
+++ SCNetworkAPI/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+  name: "SCNetworkAPI",
+  platforms: [
+    .iOS(.v12), .macOS(.v10_14)
+  ],
+  products: [
+    .library(
+      name: "SCNetworkAPI",
+      targets: ["SCNetworkAPIMobile"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "SCNetworkAPIMobile",
+      path: "Source"
+    ),
+    .testTarget(
+      name: "SCNetworkAPIMobileTests",
+      path: "Tests",
+      dependencies: [
+        "SCNetworkAPIMobile",
+        .package(url: "https://github.com/kylef/Mockingjay", .exact("3.0.0-alpha.1"))
+      ])
+  ]
+)
