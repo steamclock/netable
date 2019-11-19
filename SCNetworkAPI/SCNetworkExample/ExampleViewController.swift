11b2bbf7fcfa8aec72c1816f1a2628722f3c8890
Jenn Cooper <cooper.jennl@gmail.com>
2019-11-19 10:58:17 -0800

    Add example that utilizes defaults

--- SCNetworkAPI/SCNetworkExample/ExampleViewController.swift
+++ SCNetworkAPI/SCNetworkExample/ExampleViewController.swift
@@ -15,7 +15,7 @@ struct CatImage: Decodable {
 
 struct GetCatImageURL: Request {
     typealias Parameters = [String: String]
-    typealias Returning = [CatImage]
+    typealias RawResource = [CatImage]
     typealias FinalResource = URL
 
     public var method: HTTPMethod { return .get }
@@ -28,7 +28,7 @@ struct GetCatImageURL: Request {
         return ["mime_type": "jpg,png"]
     }
 
-    func finalize(raw: [CatImage]) -> Result<URL, NetworkAPIError> {
+    func finalize(raw: RawResource) -> Result<FinalResource, NetworkAPIError> {
         guard let catImage = raw.first else {
             return .failure(NetworkAPIError.resourceExtractionError("The CatImage array is empty"))
         }
@@ -41,8 +41,25 @@ struct GetCatImageURL: Request {
     }
 }
 
+struct GetCatImages: Request {
+    typealias Parameters = [String: String]
+    typealias RawResource = [CatImage]
+
+    public var method: HTTPMethod { return .get }
+
+    public var path: String {
+        return "images/search"
+    }
+
+    public var parameters: [String: String] {
+        return ["mime_type": "jpg,png", "limit": "2"]
+    }
+}
+
 class ExampleViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var catsImageView1: UIImageView!
+    @IBOutlet private var catsImageView2: UIImageView!
 
     private let api = NetworkAPI(baseURL: URL(string: "https://api.thecatapi.com/v1/")!)
 
@@ -58,7 +75,38 @@ class ExampleViewController: UIViewController {
 
                 self.imageView.image = UIImage(data: imageData)
             case .failure(let error):
-                let alert = UIAlertController(title: "Uh oh!", message: "Get cats request failed with error: \(error)", preferredStyle: .alert)
+                let alert = UIAlertController(
+                    title: "Uh oh!",
+                    message: "Get cat url request failed with error: \(error)",
+                    preferredStyle: .alert
+                )
+
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+
+        api.request(GetCatImages()) { result in
+            switch result {
+            case .success(let catImages):
+                if let firstCat = catImages.first,
+                   let url = URL(string: firstCat.url),
+                   let imageData = try? Data(contentsOf: url) {
+                    self.catsImageView1.image = UIImage(data: imageData)
+                }
+
+                if let lastCat = catImages.last,
+                   let url = URL(string: lastCat.url),
+                   let imageData = try? Data(contentsOf: url) {
+                    self.catsImageView2.image = UIImage(data: imageData)
+                }
+            case .failure(let error):
+                let alert = UIAlertController(
+                    title: "Uh oh!",
+                    message: "Get cats request failed with error: \(error)",
+                    preferredStyle: .alert
+                )
+
                 alert.addAction(UIAlertAction(title: "OK", style: .cancel))
                 self.present(alert, animated: true, completion: nil)
             }
