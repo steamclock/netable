6c5247e707851066ff836bff4121e891220b0415
Brendan <brendan@steamclock.com>
2021-08-20 12:48:33 -0700

    add netable-level setters for json en/decoding strategies

--- Netable/Netable/Netable.swift
+++ Netable/Netable/Netable.swift
@@ -84,7 +84,7 @@ open class Netable {
             }
 
             if T.Parameters.self != Empty.self {
-                try urlRequest.encodeParameters(for: request)
+                try urlRequest.encodeParameters(for: request, defaultEncodingStrategy: config.jsonEncodingStrategy)
             }
         } catch {
             let netableError = (error as? NetableError) ?? NetableError.unknownError(error)
@@ -107,7 +107,10 @@ open class Netable {
             urlString:  urlRequest.url?.absoluteString ?? "UNDEFINED",
             method: request.method,
             headers: urlRequest.allHTTPHeaderFields ?? [:],
-            params: try? request.parameters.toParameterDictionary(encodingStrategy: request.jsonKeyEncodingStrategy))
+            params: try? request.parameters.toParameterDictionary(
+                encodingStrategy: request.jsonKeyEncodingStrategy ?? config.jsonEncodingStrategy
+            )
+        )
 
         log(.requestStarted(request: requestInfo))
 
@@ -127,7 +130,7 @@ open class Netable {
                     throw NetableError.httpError(response.statusCode, data)
                 }
 
-                let decoded = request.decode(data)
+                let decoded = request.decode(data, defaultDecodingStrategy: self.config.jsonDecodingStrategy)
                 switch decoded {
                 case .success(let raw):
                     let finalizedData = request.finalize(raw: raw)
