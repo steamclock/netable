66dfa933da383b4278a92e81b2b0a433993f066d
Amy <amy@steamclock.com>
2022-11-23 10:25:05 -0800

    init new example project

---
+++ Netable/Example/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Amy Oulton on 2022-11-23.
+//  Copyright © 2022 Steamclock Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netable/Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Amy Oulton on 2022-11-23.
+//  Copyright © 2022 Steamclock Software. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
