5ce356e79c55a588c54fe82ae07a49c49c22f498
Brendan Lensink <brendan@steamclock.com>
2022-11-29 09:05:41 -0800

    Fix package macOS min version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Netable",
   platforms: [
-    .iOS(.v13), .macOS(.v10_14)
+    .iOS(.v13), .macOS(.v10_15)
   ],
   products: [
     .library(
